64d5b1247176fd1ffa52bee28ad6303247831dae
John Ellis <john.d.ellis01@gmail.com>
2023-01-14 12:59:30 -0500

    Initiate Shiny App

---
+++ main.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+
+
+fundraising <- read_csv("C:/dev/congress/raw/2022 election cycle fundraising.csv")
+
+states <- tibble(
+  state = state.name, 
+  state_abb = state.abb, 
+  state_area = state.area, 
+  state_lon = state.center$x, 
+  state_lat = state.center$y, 
+  state_div = state.division, 
+  state_reg = state.region)
+
+fundraising_t <- fundraising %>% 
+  tibble() %>%
+  mutate(i = as.integer(index),
+         member = str_to_upper(Member),
+         party = factor(str_to_upper(Party), labels = c('D', "I", "R")),
+         state = State,
+         chamber = factor(str_to_upper(Chamber))) %>%
+  left_join(states)
+
+glimpse(fundraising_t)
