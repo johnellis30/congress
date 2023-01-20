2cf6257081b34996dc99f36e0bf7b9bf00b4d16e
John Ellis <john.d.ellis01@gmail.com>
2023-01-20 10:41:01 -0500

    added main

--- main.R
+++ main.R
@@ -6,12 +6,12 @@ fundraising <- read_csv("C:/dev/congress/raw/2022 election cycle fundraising.csv
 
 states <- tibble(
   state = state.name, 
-  state_abb = state.abb, 
+  state_abb = factor(state.abb), 
   state_area = state.area, 
   state_lon = state.center$x, 
   state_lat = state.center$y, 
-  state_div = state.division, 
-  state_reg = state.region)
+  state_div = factor(state.division), 
+  state_reg = factor(state.region))
 
 fundraising_t <- fundraising %>% 
   tibble() %>%
@@ -19,7 +19,12 @@ fundraising_t <- fundraising %>%
          member = str_to_upper(Member),
          party = factor(str_to_upper(Party), labels = c('D', "I", "R")),
          state = State,
-         chamber = factor(str_to_upper(Chamber))) %>%
+         chamber = factor(str_to_upper(Chamber)),
+         raised =  as.numeric(gsub("[\\$,]", "", Raised)),
+         spent =  as.numeric(gsub("[\\$,]", "", Spent)),
+         cash =  as.numeric(gsub("[\\$,]", "", `Cash on Hand`)),
+         debt =  as.numeric(gsub("[\\$,]", "", Debts))) %>%
+  select(i:debt) %>%
   left_join(states)
 
 glimpse(fundraising_t)
